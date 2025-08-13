ddc0be50e02976ed1d2bc17a8605b52ee6258b42
redmage753 <firehazard159@gmail.com>
2025-08-12 22:00:57 -0500

    final submission; imperfect but functional.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,21 +8,29 @@ def get_files_info(working_directory, directory="."):
     #print(f"What's the absolute path? {os.path.abspath(directory)}")
     #print(f"is {joined_path} in {os.path.abspath(directory)}?")
     #print(f"Okay..... does it start with my /var/? {str(os.path.abspath(directory))}")
-    print(f"Trying true or false:{os.path.abspath(directory).startswith('/var/home/cmander/workspace/github.com/redmage753/agent')} ")
+    #print(f"Trying true or false:{os.path.abspath(directory).startswith('/var/home/cmander/workspace/github.com/redmage753/agent')} ")
     
     #print(f"Is it a path? {os.path.isdir(joined_path)}")
 
     try:
         #if not os.path.samepath(joined_path, os.path.abspath(directory):
         if str(os.path.abspath(directory)).startswith('/var/home/cmander/workspace/github.com/redmage753/agent') == False:
-            Exception(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+            return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
         if not os.path.isdir(joined_path):
-            Exception(f'Error: "{directory}" is not a directory')
+            return (f'Error: "{directory}" is not a directory')
         else:
             contents=os.listdir(joined_path)
-            print(contents)
+            table_list=[]
+            table_string=""
+            #print(contents)
+            for item in contents:
+                #print(os.path.getsize(os.path.join(joined_path, item)))
+                table_list.append(f"- {item}: file_size={os.path.getsize(os.path.join(joined_path, item))} bytes, is_dir={os.path.isdir(os.path.join(joined_path, item))}")
+            table_string = "\n".join(table_list)
+            #print(table_string)
+            return table_string
     except Exception as e:
-        print(f"Error encountered: {e}")
+        print(f"Error: {e}")
         return
     #contents = os.listdir(joined_path)
     #print(contents)
