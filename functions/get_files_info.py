5c59f6dcaabdd3864a21093c1abe5cdad6c8ac52
redmage753 <firehazard159@gmail.com>
2025-08-12 22:05:44 -0500

    snagged better concepts, did not clean up full script - keeping references

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,6 +4,8 @@ def get_files_info(working_directory, directory="."):
     #print(f"Work_dir: {working_directory}")
     #print(f"Dir: {directory}")
     joined_path=os.path.join(working_directory, directory)
+    absolute_working_dir = os.path.abspath(working_directory) # better solution
+    target_dir = os.path.abspath(os.path.join(working_directory, directory)) # better solution/naming
     #print(f"Trying: {joined_path}")
     #print(f"What's the absolute path? {os.path.abspath(directory)}")
     #print(f"is {joined_path} in {os.path.abspath(directory)}?")
@@ -14,7 +16,7 @@ def get_files_info(working_directory, directory="."):
 
     try:
         #if not os.path.samepath(joined_path, os.path.abspath(directory):
-        if str(os.path.abspath(directory)).startswith('/var/home/cmander/workspace/github.com/redmage753/agent') == False:
+        if not target_dir.startswith(absolute_working_dir):
             return (f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
         if not os.path.isdir(joined_path):
             return (f'Error: "{directory}" is not a directory')
